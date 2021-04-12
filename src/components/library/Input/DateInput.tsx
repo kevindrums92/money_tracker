52da7be7974cce3d4e412d267ed32719ead7f55f
Jhossept <jkgdeveloper@outlook.com>
2021-04-12 17:20:11 -0500

    datepicker solved

--- src/components/library/Input/DateInput.tsx
+++ src/components/library/Input/DateInput.tsx
@@ -1,11 +1,13 @@
 import { MaterialIcons } from '@expo/vector-icons';
 import * as React from 'react';
-import { View, StyleSheet, TouchableOpacity, Text, Button } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Text, Platform } from 'react-native';
 import { useState } from 'react';
 import CustomDatePicker from '../CustomDatePicker';
 import { customInputStyles } from './Styles';
 import { toCamelCase } from '../../../utils/stringUtils';
 import { formatDate } from '../../../utils/date';
+import DateTimePicker from '@react-native-community/datetimepicker';
+
 
 interface DateInputProps {
     onChange: (...event: any[]) => void;
@@ -28,10 +30,22 @@ const DateInput = (props: DateInputProps) => {
                 <View style={customInputStyles.textInput}>
                     <Text style={customInputStyles.textInputText}>{toCamelCase(formatDate(props.value))}</Text>
                 </View>
-                <CustomDatePicker value={props.value} modalVisible={isPickerVisible}
+                {Platform.OS === 'ios' && <CustomDatePicker value={props.value} modalVisible={isPickerVisible}
                     closeControl={() => {
                         setIsPickerVisible(false);
-                    }} onChange={props.onChange} />
+                    }} onChange={props.onChange} />}
+                {/* Content */}
+                {isPickerVisible && Platform.OS === 'android' && <DateTimePicker
+                    value={props.value}
+                    mode="date"
+                    textColor='red'
+                    onChange={(event, selectedDate) => {
+                        setIsPickerVisible(false);
+                        if (selectedDate) {
+                            props.onChange(selectedDate);
+                        }
+                    }}
+                />}
             </View>
             {props.errorField}
         </TouchableOpacity>
